cdb6fb5042ad167b8c6d52209656962df022a2f8
duanran <995122760@qq.com>
2023-07-18 20:24:25 +0800

    try first cmake

---
+++ example/callee/userService.cc
@@ -0,0 +1,31 @@
+#include<iostream>;
+#include<string>;
+#include"user.pb.h";
+
+class UserService : public::DUAN::UserServiceRpc
+{
+    public:
+        bool Login(std::string name, std::string password)
+        {
+            std::cout << "name : " << name << "password : " << password << std::endl;
+            return true;
+        }
+        
+        void Login(::google::protobuf::RpcController* controller,
+                    const ::DUAN::LoginRequest* request,
+                    ::DUAN::LoginResponse* response,
+                    ::google::protobuf::Closure* done)
+        {
+            std::string name = request->name();
+            std::string password = request->password();
+
+            bool success = Login(name, password);
+
+            DUAN::ResultMsg *msg = response->mutable_msg();
+            msg->set_statecode(200);
+            msg->set_msg("good");
+            response->set_success(success);
+
+            done->Run();
+        }
+};
